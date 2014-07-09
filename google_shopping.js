a29d489533a97209d2cd5c16a5bca004835467cc
Rakulon <kekknyx@gmail.com>
2014-07-08 23:04:43 -0700

    Update google_shopping.js
    test

--- google_shopping.js
+++ google_shopping.js
@@ -1,5 +1,14 @@
 var data = require("./products.json")
 
 // Write your solutions below
+var data = require("./products.json")
+
+// Write your solutions below
+for (i = 0; i < data.items.length; i++) {
+ 	if (data.items[i]["kind"] === "shopping#product") {
+ 		count++;
+ 	}
+ }
+console.log(count)
 
 console.log(data["items"]);
