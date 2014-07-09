8e1203097e3625ca25e92febcda6faca43c60cea
Rakulon <kekknyx@gmail.com>
2014-07-09 12:14:26 -0700

    Figured out trying to pull mine up that I did not have this in the correct file for the original upload. Commiting the word filter.

---
+++ filterlong.js
@@ -0,0 +1,6 @@
+var words = ["Blackbox","Thing","The","Preceptor","Battleship"]
+var maxLength = "6"
+for (var i = 0; i < words.length; i++) {
+if(words[i].length < maxLength) 
+	console.log(words[i]);
+}
